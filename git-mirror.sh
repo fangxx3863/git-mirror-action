c515a9cda5eb9bcd89023aeecc16bd19b9f8b30c
fangxx3863 <48589001+fangxx3863@users.noreply.github.com>
2022-01-27 16:29:31 +0800

    Update git-mirror.sh

--- git-mirror.sh
+++ git-mirror.sh
@@ -31,6 +31,59 @@ cd Origin
 rm -rf .git
 rm -rf .github
 rm update
+
+cd ONywPfH3Rh
+FILE=pWgXrfGEV6Vtmhy
+SIZE=12000
+# 数据总条数, 去掉wc前面的空格
+ROWS=`cat $FILE | wc -l | sed 's/ //g'`
+# 向下取整数(floor)
+FLOOR=$(( $ROWS/$SIZE  ))
+# 向上取整数(ceil)
+CEIL=`echo "if ( $ROWS%$SIZE  ) $ROWS/$SIZE+1 else $ROWS/$SIZE" | bc`
+# 文件名前缀
+FILE_PREFIX=${FILE%.*}
+echo "file:$FILE prefix:$FILE_PREFIX size:$SIZE rows:$ROWS FLOOR:$FLOOR CEIL:$CEIL"
+# 先按整除的迭代
+for i in `seq 1 $FLOOR`; do
+    LOAD_CNT=$((i*$SIZE))
+    echo "head -$LOAD_CNT $FILE | tail -$SIZE > ${FILE_PREFIX}_$i.part1"
+    # execute split file
+    head -$LOAD_CNT $FILE | tail -$SIZE > ${FILE_PREFIX}_$i.part1
+done
+# 如果最后还有余数
+if [[ $FLOOR != $CEIL ]]; then
+    LEFT_ROWS=$(( $ROWS - $FLOOR*$SIZE ))
+    echo "tail -$LEFT_ROWS $FILE > ${FILE_PREFIX}_$CEIL.part1"
+    tail -$LEFT_ROWS $FILE > ${FILE_PREFIX}_$CEIL.part1
+fi
+
+FILE=AlCdr6hj5qHCJmp
+SIZE=12000
+# 数据总条数, 去掉wc前面的空格
+ROWS=`cat $FILE | wc -l | sed 's/ //g'`
+# 向下取整数(floor)
+FLOOR=$(( $ROWS/$SIZE  ))
+# 向上取整数(ceil)
+CEIL=`echo "if ( $ROWS%$SIZE  ) $ROWS/$SIZE+1 else $ROWS/$SIZE" | bc`
+# 文件名前缀
+FILE_PREFIX=${FILE%.*}
+echo "file:$FILE prefix:$FILE_PREFIX size:$SIZE rows:$ROWS FLOOR:$FLOOR CEIL:$CEIL"
+# 先按整除的迭代
+for i in `seq 1 $FLOOR`; do
+    LOAD_CNT=$((i*$SIZE))
+    echo "head -$LOAD_CNT $FILE | tail -$SIZE > ${FILE_PREFIX}_$i.part1"
+    # execute split file
+    head -$LOAD_CNT $FILE | tail -$SIZE > ${FILE_PREFIX}_$i.part1
+done
+# 如果最后还有余数
+if [[ $FLOOR != $CEIL ]]; then
+    LEFT_ROWS=$(( $ROWS - $FLOOR*$SIZE ))
+    echo "tail -$LEFT_ROWS $FILE > ${FILE_PREFIX}_$CEIL.part1"
+    tail -$LEFT_ROWS $FILE > ${FILE_PREFIX}_$CEIL.part1
+fi
+cd ..
+
 cd ..
 cp -rf Origin/. Target/
 
